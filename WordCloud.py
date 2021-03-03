2833b15d8c07f8bd092fb8395e9f0760a7cba701
rang21c <rang21c@naver.com>
2021-03-03 18:32:09 +0900

    주석 추가

--- WordCloud.py
+++ WordCloud.py
@@ -9,8 +9,10 @@ with open("kakaotalk.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
     for line in lines:
         if '] [' in line:
-            text += line.split('] ')[2].replace('ㅋ','').replace('ㅠ','').replace('ㅜ','').replace('사진\n','').replace('이모티콘','').replace('JJANGA','')
+            #데이터 클렌징
+            text += line.split('] ')[2].replace('ㅋ','').replace('ㅠ','').replace('ㅜ','').replace('사진\n','').replace('이모티콘','')
 
+#폰트 선택
 font_path = 'C:/Windows/Fonts/malgunbd.ttf'
 
 mask = np.array(Image.open('cloud.png'))
