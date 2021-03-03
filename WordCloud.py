a9b9e6bde8fc52bb22a194ddc2ea76ccccf85bdd
rang21c <rang21c@naver.com>
2021-03-03 18:28:23 +0900

    WordCloud

---
+++ WordCloud.py
@@ -0,0 +1,19 @@
+import matplotlib.font_manager as fm
+from wordcloud import WordCloud
+from PIL import Image
+import numpy as np
+
+
+text = ""
+with open("kakaotalk.txt", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        if '] [' in line:
+            text += line.split('] ')[2].replace('ㅋ','').replace('ㅠ','').replace('ㅜ','').replace('사진\n','').replace('이모티콘','').replace('JJANGA','')
+
+font_path = 'C:/Windows/Fonts/malgunbd.ttf'
+
+mask = np.array(Image.open('cloud.png'))
+wc = WordCloud(font_path=font_path, background_color="white", mask=mask)
+wc.generate(text)
+wc.to_file("result_wordcloud.png")
